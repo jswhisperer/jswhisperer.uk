944f4d1d7b58ec940780f6103313c33eb118f40b
gregpalaci <gregory.palaci.uk@gmail.com>
2025-01-15 05:26:34 +0000

    a

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,6 @@
 import { siteConfig } from '@/site-config'
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import atom from './atom'
 
 export async function GET(context: any) {
 	const posts = await getCollection('blog')
@@ -12,24 +11,26 @@ export async function GET(context: any) {
 		...post.data,
 		title: post.data.title,
 		categories: post.data.tags,
-		xmlns: {
-			atom: 'http://www.w3.org/2005/Atom'
-		},
+
 		link: `/post/${post.slug}/`,
 		pubDate: post.data.pubDate,
 		//content: sanitizeHtml(parser.render(post.body)),
-		customData: [
-			'en-us',
-			`<atom:link rel="self" type="application/rss+xml" href="${siteConfig.site}/feed.xml" />`
-		].join(''),
+
 		author: 'hello@jswhisperer.uk' // currentAuthor(post)?.email
 	}))
 
-	await atom()
+	// await atom()
 	return await rss({
+		xmlns: {
+			atom: 'http://www.w3.org/2005/Atom'
+		},
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: context.site,
-		items: await Promise.all(items)
+		items: await Promise.all(items),
+		customData: [
+			'en-us',
+			`<atom:link rel="self" type="application/rss+xml" href="${siteConfig.site}/feed.xml" />`
+		].join('')
 	})
 }
