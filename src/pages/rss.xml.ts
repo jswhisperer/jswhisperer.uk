0f4923e2963566135db8edae2df4ad0988a2a882
gregpalaci <gregory.palaci.uk@gmail.com>
2025-01-15 04:47:44 +0000

    a

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,28 +3,6 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import atom from './atom'
 
-// const slugDate = function (date: string) {
-// 	const m = moment(date)
-// 	date = `${m.format('YYYY')}/${m.format('MM')}/${m.format('DD')}/`
-// 	return date
-// }
-
-// const createSlug = function (title: string) {
-// 	return (
-// 		title
-// 			// remove leading & trailing whitespace
-// 			.trim()
-// 			// remove special characters
-// 			.replace(/[^A-Za-z0-9 ]/g, '')
-// 			// replace spaces
-// 			.replace(/\s+/g, '-')
-// 			// remove leading & trailing separtors
-// 			.replace(/^-+|-+$/g, '')
-// 			// output lowercase
-// 			.toLowerCase()
-// 	)
-// }
-
 export async function GET(context: any) {
 	const posts = await getCollection('blog')
 	const authors = await getCollection('author')
@@ -46,10 +24,5 @@ export async function GET(context: any) {
 		description: siteConfig.description,
 		site: context.site,
 		items: await Promise.all(items)
-		// customData: [
-		// 	'en-us',
-		// 	`<atom:link rel="self" type="application/rss+xml" href="${siteConfig.site}/feed.xml" />`
-		// ].join(''),
-		// stylesheet: '/pretty-feed-v3.xsl'
 	})
 }
