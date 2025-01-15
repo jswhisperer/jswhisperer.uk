b516b6ca98bd98df54680873e7e4696134db4278
gregpalaci <gregory.palaci.uk@gmail.com>
2025-01-15 00:59:45 +0000

    a

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,16 +2,29 @@ import { siteConfig } from '@/site-config'
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
+const parser = new MarkdownIt()
+
 export async function GET(context: any) {
 	const posts = await getCollection('blog')
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			author: post.data.author.id,
-			link: `post/${post.slug}/`
-		}))
+		items: await Promise.all(
+			posts.map(async (post) => {
+				const authors = await getCollection('author')
+				const author = authors.find((author) => author.id === post.data.author.id)
+				return {
+					...post.data,
+					content: sanitizeHtml(parser.render(post.body), {
+						allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+					}),
+					author: author ? author.data.name : 'Unknown',
+					link: `post/${post.slug}/`
+				}
+			})
+		)
 	})
 }
