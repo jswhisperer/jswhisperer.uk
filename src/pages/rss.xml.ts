326d7ad3d8edbef2a717a42c8eee70c5e932ddda
gregpalaci <gregory.palaci.uk@gmail.com>
2025-01-15 02:23:35 +0000

    a

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,30 +1,29 @@
 import { siteConfig } from '@/site-config'
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
 
 import MarkdownIt from 'markdown-it'
-import sanitizeHtml from 'sanitize-html'
 const parser = new MarkdownIt()
 
 export async function GET(context: any) {
 	const posts = await getCollection('blog')
+	const authors = await getCollection('author')
+	const items = posts.map((post) => ({
+		...post.data,
+		title: post.data.title,
+		pubDate: post.data.pubDate,
+		content: sanitizeHtml(parser.render(post.body)),
+		author: authors.find((author) => author.data.name === post.data.author.id)?.data.name,
+		link: `/post/${post.slug}/`
+	}))
+
+	console.log({ items: await Promise.all(items) })
+
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: context.site,
-		items: await Promise.all(
-			posts.map(async (post) => {
-				const authors = await getCollection('author')
-				const author = authors.find((author) => author.id === post.data.author.id)
-				return {
-					...post.data,
-					content: sanitizeHtml(parser.render(post.body), {
-						allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-					}),
-					author: author ? author.data.name : 'Unknown',
-					link: `post/${post.slug}/`
-				}
-			})
-		)
+		items: items
 	})
 }
