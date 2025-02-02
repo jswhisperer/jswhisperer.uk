bc1bf9bc1b845f368fec781c55a5e8943415b97d
Gregory JSWhisperer <hello@jswhisperer.uk>
2025-02-02 18:35:07 +0000

    x

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,12 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
+declare module 'astro-imagetools/components'
+declare module 'astro-imagetools'
+declare module 'astro-imagetools/transform'
+declare module 'astro-imagetools/transformers'
+declare module 'astro-imagetools/transformers/transformer'
+declare module 'astro-imagetools/transformers/transformer-args'
+/// declare module 'astro-imagetools/transformers/transformer-options';
+/// declare module 'astro-imagetools/transformers/transformer-result';
+/// declare module 'astro-imagetools/transformers/transformer-type';
