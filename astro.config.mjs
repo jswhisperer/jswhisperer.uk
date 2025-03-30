1c4d76529022481d602919806dd62fc3132a5ab0
Gregory JSWhisperer <hello@jswhisperer.uk>
2025-03-30 11:23:29 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
+	prefetch: {
+		defaultStrategy: 'viewport'
+	  },
 	// image: {
 	// 	// service: {
 	// 	// 	entrypoint: 'astro/assets/services/squoosh'
